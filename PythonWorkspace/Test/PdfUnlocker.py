bf1f0f0b99377d1841044f6d00baab23c1aef170
Justin Lin <tony199462@gmail.com>
2015-09-28 14:32:13 +0800

    add pdf viewer

--- PythonWorkspace/Test/PdfUnlocker.py
+++ PythonWorkspace/Test/PdfUnlocker.py
@@ -1,4 +1,20 @@
-import pyPdf
+from pdfrw import PdfReader
 
-__author__ = 'Justin'
-pdf = pyPdf.PdfFileReader(open('GRE OV.pdf', "rb"))
+file = PdfReader('GRE OV.pdf')
+
+#print(file.Info)
+
+'''
+print(file.Root)
+f = open('txtfile/Root file.txt','w',encoding='UTF-8')
+for s in file.Root:
+    f.writelines(s)
+f.close()
+'''
+
+print(file.Encrypt)
+f = open('txtfile/Encrypt file.txt','w',encoding='UTF-8')
+fencrypt = print(file.Encrypt)
+for s in fencrypt:
+    f.writelines(s)
+f.close()
--- PythonWorkspace/Test/Test.py
+++ PythonWorkspace/Test/Test.py
@@ -1,8 +1,6 @@
 __author__ = 'Justin'
 
 
-string = "1 2 3 4 5 6 7 8 9 10"
-char = string.split(" ")
-print(char[0])
-t = int(char[0]) +1
-print(t)
+t = {'/Length': '128', '/Filter': '/Standard', '/V': '4', '/EncryptMetadata': 'true'}
+for z in t:
+    print(t.get(z))
